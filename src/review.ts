eda6ce3612f70575435085c2ba6ffb2cee97829d
WAKO@program <tongarisaibou@gmail.com>
2024-01-28 00:23:03 +0900

    :truck: doSummaryをメイン関数外に切り出し

--- src/review.ts
+++ src/review.ts
@@ -296,79 +296,13 @@ ${hunks.oldHunk}
     return
   }
 
-  const summariesFailed: string[] = []
-
-  const doSummary = async (
-    filename: string,
-    fileContent: string,
-    fileDiff: string
-  ): Promise<[string, string, boolean] | null> => {
-    info(`summarize: ${filename}`)
-    const ins = inputs.clone()
-    if (fileDiff.length === 0) {
-      warning(`summarize: file_diff is empty, skip ${filename}`)
-      summariesFailed.push(`${filename} (empty diff)`)
-      return null
-    }
-
-    ins.filename = filename
-    ins.fileDiff = fileDiff
-
-    // render prompt based on inputs so far
-    const summarizePrompt = prompts.renderSummarizeFileDiff(
-      ins,
-      options.reviewSimpleChanges
-    )
-    const tokens = getTokenCount(summarizePrompt)
-
-    if (tokens > options.lightTokenLimits.requestTokens) {
-      info(`summarize: diff tokens exceeds limit, skip ${filename}`)
-      summariesFailed.push(`${filename} (diff tokens exceeds limit)`)
-      return null
-    }
-
-    // summarize content
-    try {
-      const [summarizeResp] = await lightBot.chat(summarizePrompt, {})
-
-      if (summarizeResp === '') {
-        info('summarize: nothing obtained from openai')
-        summariesFailed.push(`${filename} (nothing obtained from openai)`)
-        return null
-      } else {
-        if (options.reviewSimpleChanges === false) {
-          // parse the comment to look for triage classification
-          // Format is : [TRIAGE]: <NEEDS_REVIEW or APPROVED>
-          // if the change needs review return true, else false
-          const triageRegex = /\[TRIAGE\]:\s*(NEEDS_REVIEW|APPROVED)/
-          const triageMatch = summarizeResp.match(triageRegex)
-
-          if (triageMatch != null) {
-            const triage = triageMatch[1]
-            const needsReview = triage === 'NEEDS_REVIEW'
-
-            // remove this line from the comment
-            const summary = summarizeResp.replace(triageRegex, '').trim()
-            info(`filename: ${filename}, triage: ${triage}`)
-            return [filename, summary, needsReview]
-          }
-        }
-        return [filename, summarizeResp, true]
-      }
-    } catch (e: any) {
-      warning(`summarize: error from openai: ${e as string}`)
-      summariesFailed.push(`${filename} (error from openai: ${e as string})})`)
-      return null
-    }
-  }
-
   const summaryPromises = []
   const skippedFiles = []
-  for (const [filename, fileContent, fileDiff] of filesAndChanges) {
+  for (const [filename, fileDiff] of filesAndChanges) {
     if (options.maxFiles <= 0 || summaryPromises.length < options.maxFiles) {
       summaryPromises.push(
         openaiConcurrencyLimit(
-          async () => await doSummary(filename, fileContent, fileDiff)
+          async () => await doSummary({ filename, fileDiff, options, inputs, prompts, lightBot })
         )
       )
     } else {
@@ -916,4 +850,74 @@ ${review.comment}`
   storeReview()
 
   return reviews
+}
+
+const doSummary = async ({
+  filename,
+  fileDiff,
+  options,
+  inputs,
+  prompts,
+  lightBot
+} : {
+  filename: string;
+  fileDiff: string;
+  options: Options;
+  inputs: Inputs;
+  prompts: Prompts;
+  lightBot: Bot;
+}): Promise<[string, string, boolean] | null> => {
+  info(`summarize: ${filename}`)
+  const ins = inputs.clone()
+  if (fileDiff.length === 0) {
+    warning(`summarize: file_diff is empty, skip ${filename}`)
+    return null
+  }
+
+  ins.filename = filename
+  ins.fileDiff = fileDiff
+
+  // インプットに基づいてプロンプトをレンダリングする
+  const summarizePrompt = prompts.renderSummarizeFileDiff(
+    ins,
+    options.reviewSimpleChanges
+  )
+  const tokens = getTokenCount(summarizePrompt)
+
+  if (tokens > options.lightTokenLimits.requestTokens) {
+    info(`summarize: diff tokens exceeds limit, skip ${filename}`)
+    return null
+  }
+
+  // コンテキストを要約する
+  try {
+    const [summarizeResp] = await lightBot.chat(summarizePrompt, {})
+
+    if (summarizeResp === '') {
+      info('summarize: nothing obtained from openai')
+      return null
+    } else {
+      if (options.reviewSimpleChanges === false) {
+        // 分類をトリアージするためにコメントを解析する
+        // フォーマットは: [TRIAGE]: <NEEDS_REVIEW or APPROVED>
+        // 変更がレビューを必要とする場合はtrue、それ以外はfalseを返します。
+        const triageRegex = /\[TRIAGE\]:\s*(NEEDS_REVIEW|APPROVED)/
+        const triageMatch = summarizeResp.match(triageRegex)
+
+        if (triageMatch != null) {
+          const triage = triageMatch[1]
+          const needsReview = triage === 'NEEDS_REVIEW'
+
+          // この行をコメントから削除します。
+          const summary = summarizeResp.replace(triageRegex, '').trim()
+          info(`filename: ${filename}, triage: ${triage}`)
+          return [filename, summary, needsReview]
+        }
+      }
+      return [filename, summarizeResp, true]
+    }
+  } catch (e: any) {
+    warning(`summarize: error from openai: ${e as string}`)
+    return null
+  }
 }
